210ca994773beec356930e75f4b0301df4ebf88c
Aswini Kumar <aswini0509@gmail.com>
2025-03-20 18:44:48 +0530

    prompt update

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def create_vector_embedding():
         st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
         st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
         st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
-st.title("RAG Document Q&A With Groq")
+st.title("LAWS Document Question Answering System")
 
 user_prompt=st.text_input("Enter your query")
 
