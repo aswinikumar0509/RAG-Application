7649bcf752f2685eaeae82a0272531d4e21c2623
Aswini Kumar <aswini0509@gmail.com>
2025-04-19 18:51:34 +0530

    update api

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
 ## load the GROQ API Key
-os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
+# os.environ['GROQ_API_KEY']=os.getenv("GROQ_API_KEY")
 groq_api_key=os.getenv("GROQ_API_KEY")
 
 
